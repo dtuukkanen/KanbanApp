caca630de6c7db7bae7472adcd5fbc3768b20207
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-02-10 17:48:38 +0200

    Columns can be deleted

---
+++ server/src/routes/deleteColumn.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { KanbanCardModel } from '../models/KanbanCard';
+import { KanbanColumnModel } from '../models/KanbanColumn';
+
+const createColumnRouter = Router();
+
+createColumnRouter.delete('/', async (req, res) => {
+    try {
+        const { columnId } = req.body;
+    
+        const column = await KanbanColumnModel.findById(columnId);
+        if (!column) {
+            return void res.status(404).json({ message: "Column not found" });
+        } else {
+            await KanbanCardModel.deleteMany({ _id: { $in: column.cardIds } });
+            await column.deleteOne();
+            return void res.status(200).json({ message: "Column deleted" });
+        }
+    } catch (error: any) {
+        console.error("Error deleting column:", error);
+        return void res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+export default createColumnRouter;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import addCardRouter from './addCard';
 import createColumnRouter from './createColumn';
+import deleteColumnRouter from './deleteColumn';
 
 // Create a new router to handle all the routes
 const router = Router();
@@ -8,6 +9,7 @@ const router = Router();
 // Define the routes
 router.use('/addCard', addCardRouter);
 router.use('/createColumn', createColumnRouter);
+router.use('/deleteColumn', deleteColumnRouter);
 
 // Export the router
 export default router;
