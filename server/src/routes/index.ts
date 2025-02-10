8f0599639901d58bd1c8b5c5138a61341d6bb863
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-02-10 18:08:48 +0200

    Columns can be renamed

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import addCardRouter from './addCard';
 import createColumnRouter from './createColumn';
 import deleteColumnRouter from './deleteColumn';
+import renameColumnRouter from './renameColumn';
 
 // Create a new router to handle all the routes
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 router.use('/addCard', addCardRouter);
 router.use('/createColumn', createColumnRouter);
 router.use('/deleteColumn', deleteColumnRouter);
+router.use('/renameColumn', renameColumnRouter);
 
 // Export the router
 export default router;
---
+++ server/src/routes/renameColumn.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { KanbanColumnModel } from '../models/KanbanColumn';
+
+const renameColumnRouter = Router();
+
+renameColumnRouter.put('/', async (req, res) => {
+    try {
+        const { columnId, newTitle } = req.body;
+    
+        const column = await KanbanColumnModel.findById(columnId);
+        if (!column) {
+            return void res.status(404).json({ message: "Column not found" });
+        } else {
+            column.title = newTitle;
+            await column.save();
+            return void res.status(200).json({ message: "Column renamed successfully" });
+        }
+    } catch (error: any) {
+        console.error("Error deleting column:", error);
+        return void res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+export default renameColumnRouter;
