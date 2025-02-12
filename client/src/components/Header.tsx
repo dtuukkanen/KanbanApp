833eb2568094b445eb7ee37ab04705ffa25ac84d
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-02-12 13:46:00 +0200

    Header done

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="mx-auto max-w-2xl text-center">
+      <p className="text-base/7 font-semibold text-indigo-600">USERNAME</p>
+      <h2 className="mt-2 text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Kanban Board</h2>
+      <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+        This might have some text or it might not. Who knows? It's a mystery.
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default Header
