22849effcb5ab29d12a84d07c85eab73e44f50ff
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-02-13 11:20:22 +0200

    Header created and old header changed to welcome

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
 import Board from './components/Board'
+import Welcome from './components/Welcome'
 import Header from './components/Header'
 import Login from './components/Login'
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
       <Header />
-      <Board  />
-      <Login />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/board" element={<Board />} /> 
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
