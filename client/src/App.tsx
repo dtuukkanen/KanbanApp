f82d6eb516de9fb9f95b24c7252c842875d69ac2
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-02-13 12:37:26 +0200

    Account register component created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Board from './components/Board'
 import Welcome from './components/Welcome'
 import Header from './components/Header'
 import Login from './components/Login'
+import Register from './components/Register'
 import { Routes, Route } from 'react-router-dom'
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/board" element={<Board />} /> 
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   )
